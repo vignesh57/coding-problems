dcdd159faeb82a68b969623f61f2598d315f6af9
Vignesh Dhakshinamoorthy <vignesh.professional@gmail.com>
2020-10-17 14:32:43 -0700

    Initial push #3

--- search-2D-matrix.js
+++ search-2D-matrix.js
@@ -1,8 +1,11 @@
-/*source: https://leetcode.com/explore/challenge/card/october-leetcoding-challenge/561/week-3-october-15th-october-21st/3497/
-Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
 
+/*
+search-2D-matrix.js
+source: https://leetcode.com/explore/challenge/card/october-leetcoding-challenge/561/week-3-october-15th-october-21st/3497/
+Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
 Integers in each row are sorted from left to right.
 The first integer of each row is greater than the last integer of the previous row.
+
 */
 
 /**
